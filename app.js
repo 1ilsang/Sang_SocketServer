a3c0f0b9963421484d5de0a2c3cfd44f71e768d9
sangchul <1ilsang@naver.com>
2018-10-04 05:04:57 +0900

    Socket on Redis

--- app.js
+++ app.js
@@ -1,46 +1,29 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const io = require('socket.io').listen(3113);
+const redis = require('redis');
+const config = require('./config');
+const pub = redis.createClient(config.redisPort, config.redisHost);
+const sub = redis.createClient(config.redisPort, config.redisHost);
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+sub.subscribe('chat');
 
-var app = express();
+function sendPublish(channel, type, message){
+    let socketTemplate = {
+        type: type,
+        message: message,
+    };
+    pub.publish(channel, JSON.stringify(socketTemplate));
+}
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+io.sockets.on('connection', (socket) => {
+  socket.on('join', () => {
+      sendPublish('chat', 'join');
+  });
+  socket.on('message', (msg) => {
+      sendPublish('chat', 'message', msg);
+  });
+  sub.on('message', (channel, jsonData) => {
+      let data = JSON.parse(jsonData);
+      if(data.type === 'join') socket.emit('message', ' : : : 익명의 누군가가 접속했습니다! : : :');
+      else if(data.type === 'message') socket.emit('message', data.message);
+  });
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    redisPort: '6379',
+    redisHost: 'localhost'
+}
+module.exports = config;
